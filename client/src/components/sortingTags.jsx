97fbc714b86a43b61b20765e1b96b14e099accad
vishal yadav <vishal@progryss.com>
2025-02-11 14:41:04 +0530

    adding notification in sort tags

--- client/src/components/sortingTags.jsx
+++ client/src/components/sortingTags.jsx
@@ -1,25 +1,30 @@
 import { Button } from '@mui/material';
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import { useProgressToast } from "./customHooks/useProgressToast";
 
 const serverUrl = process.env.REACT_APP_SERVER_URL;
 function SortingTags() {
+    const { showProgressToast, updateProgress, finalizeToast, setProgress } = useProgressToast();
+    const [value, setValue] = useState('')
 
-    const [value,setValue] = useState('')
-
-    const updateSortingTags = async()=>{
+    const updateSortingTags = async () => {
+        const toastId = showProgressToast('Saving Data...');
+        updateProgress(toastId, 'loader', 'Saving Data...');
         const payload = {
-            sortingTags : value
+            sortingTags: value
         }
         try {
-            const response = await axios.put(`${serverUrl}/api/update-sorting-tags`,payload)
+            const response = await axios.put(`${serverUrl}/api/update-sorting-tags`, payload)
             console.log(response)
+            finalizeToast(toastId, true, "Data Saved Successfully!");
         } catch (error) {
+            finalizeToast(toastId, false, "Failed to save data.");
             console.log(error)
         }
     }
 
-    const getSortingTags = async()=>{
+    const getSortingTags = async () => {
         try {
             const response = await axios.get(`${serverUrl}/api/sorting-tags`);
             setValue(response.data[0].sortTag)
@@ -27,13 +32,13 @@ function SortingTags() {
             console.log(error)
         }
     }
-    useEffect(()=>{
+    useEffect(() => {
         getSortingTags()
-    },[])
+    }, [])
     return (
         <div className='mt-4 ms-2'>
-            <p>Sorting Tags :</p>
-            <textarea onChange={e=>setValue(e.target.value)} name="w3review" rows="3" cols="50" value={value} placeholder='Add tags here...' style={{ padding: '10px' }} />
+            <p>Sorting Tags : (Add multiple product tags with comma "," separator)</p>
+            <textarea onChange={e => setValue(e.target.value)} name="w3review" rows="10" value={value} placeholder='Add tags here...' style={{ padding: '10px', width: "calc(100% - 10px )" }} />
             <div>
                 <Button variant='contained' className='mt-3' onClick={updateSortingTags}>Update</Button>
             </div>
